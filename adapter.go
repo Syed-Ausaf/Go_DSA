0c95a83864fdd017542b75538cb8203d0d499824
Syed Ausaf Haider <101732958+Syed-Ausaf@users.noreply.github.com>
2022-06-21 19:47:01 +0530

    Update adapter.go

--- adapter.go
+++ adapter.go
@@ -1,43 +1,28 @@
-//main package has examples shown
-// in Go Data Structures and algorithms book
 package main
-
-// importing fmt package
 import (
 	"fmt"
 )
 
-//IProces interface
-type IProcess interface {
+type IProcess interface {        //IProces interface
 	process()
 }
-
-//Adapter struct
-type Adapter struct {
+type Adapter struct {          //Adapter struct
 	adaptee Adaptee
 }
-
-//Adapter class method process
-func (adapter Adapter) process() {
+func (adapter Adapter) process() {     //Adapter class method process
 	fmt.Println("Adapter process")
 	adapter.adaptee.convert()
 }
 
-//Adaptee Struct
-type Adaptee struct {
+type Adaptee struct { //Adaptee Struct
 	adapterType int
 }
 
-// Adaptee class method convert
-func (adaptee Adaptee) convert() {
+func (adaptee Adaptee) convert() {                     // Adaptee class method convert
 	fmt.Println("Adaptee convert method")
 }
 
-// main method
 func main() {
-
 	var processor IProcess = Adapter{}
-
 	processor.process()
-
 }
